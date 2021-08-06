f44b77e175273ace87845c0a5041dd626f898777
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2021-08-06 12:27:00 -0300

    Adding Sort and Cache

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 import { Member } from '../_models/member';
 import { environment } from '../../environments/environment';
+import { PaginatedResult } from '../_models/pagination';
+import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +16,56 @@ import { environment } from '../../environments/environment';
 export class MembersService {
   readonly baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private _http: HttpClient) { }
-
-  getMembers() {
-    if (this.members.length > 0) return of(this.members);
-    return this._http.get<Member[]>(`${this.baseUrl}/users`).pipe(
-      map(members => {
-        this.members = members;
-        return members;
-      })
-    );
-
+  constructor(private _http: HttpClient, private _accountService: AccountService) {
+    this._accountService.currentUser$.pipe(take(1)).subscribe(user => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    })
   }
 
+  getUserParams() {
+    return this.userParams;
+  }
+
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
+
+  resetUserParams() {
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
+
+  getMembers(userParams: UserParams) {
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if (response) {
+      return of(response);
+    }
+    let params = this.getPaginationHeaders(userParams.pageNumber, userParams.pageSize);
+    params = params.append('minAge', userParams.minAge.toString());
+    params = params.append('maxAge', userParams.maxAge.toString());
+    params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy);
+
+    return this.getPaginatedResult<Member[]>(`${this.baseUrl}/users/`, params)
+      .pipe(map(response => { //Set Cache Values of Member[]
+        this.memberCache.set(Object.values(userParams).join('-'), response);
+        return response;
+      }));
+  }
+
+
   getMember(username: string) {
-    const member = this.members.find(x => x.username === username);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+      .reduce((arr, element) => arr.concat(element.result), [])
+      .find((member: Member) => member.username === username);
+
+    if (member) return of(member);
+
     return this._http.get<Member>(`${this.baseUrl}/users/${username}`);
   }
 
@@ -47,5 +84,24 @@ export class MembersService {
 
   deletePhoto(photId: number) {
     return this._http.delete(`${this.baseUrl}/users/delete-photo/${photId}`);
+  }
+
+  private getPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+    params = params.append('pageNumber', pageNumber.toString()).append('pageSize', pageSize.toString());
+    return params;
+  }
+
+  private getPaginatedResult<T>(url, params) {
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
+    return this._http.get<T>(url, { observe: 'response', params }).pipe(
+      map(response => {
+        paginatedResult.result = response.body;
+        if (response.headers.get('Pagination') !== null) {
+          paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+        }
+        return paginatedResult;
+      })
+    );
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { Member } from '../../_models/member';
+import { Pagination } from '../../_models/pagination';
+import { UserParams } from '../../_models/userParams';
+import { User } from '../../_models/user';
 import { MembersService } from '../../_services/members.service';
 
 @Component({
@@ -10,11 +13,36 @@ import { MembersService } from '../../_services/members.service';
   styleUrls: ['./member-list.component.scss']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  userParams: UserParams;
+  user: User;
+  genderList = [{ value: 'male', display: 'Male' }, { value: 'female', display: 'Female' }];
 
-  constructor(private _membersService: MembersService) { }
+  constructor(private _membersService: MembersService) {
+    this.userParams = this._membersService.getUserParams();
+  }
 
   ngOnInit(): void {
-    this.members$ = this._membersService.getMembers()  
-  } 
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this._membersService.setUserParams(this.userParams);
+    this._membersService.getMembers(this.userParams).subscribe(response => {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  pageChanged(event: any) {
+    this.userParams.pageNumber = event.page;
+    this._membersService.setUserParams(this.userParams);
+    this.loadMembers();
+  }
+
+  resetFilters() {
+    this.userParams = this._membersService.resetUserParams();
+    this.loadMembers();
+  }
 }
